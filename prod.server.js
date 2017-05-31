150dbe26e8d43bf1e507f43cce7bfa76fa4f070d
chen <179023084@qq.com>
2017-05-31 16:49:51 +0800

    change prod

--- prod.server.js
+++ prod.server.js
@@ -1,7 +1,55 @@
-/**
- * @author: chen
- * @description:
- * @Date: 2017/5/29 下午12:57
- */
 var express = require('express');
-var config = required('')
+var config = require('./config/index');
+
+var port = process.env.PORT || config.build.port;
+
+
+var app = express();
+
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+  req.url = '/index.html';
+  next();
+});
+
+app.use(router);
+
+var appData = require('./data.json');
+var seller = appData.seller;
+var goods = appData.goods;
+var ratings = appData.ratings;
+
+
+var apiRouter = express.Router();
+
+apiRouter.get('./seller', function (req, res) {
+  res.json({
+    errno: 0,
+    data: seller
+  });
+});
+
+apiRouter.get('/goods', function (req, res) {
+  res.json({
+    errno: 0,
+    data: goods
+  });
+});
+
+apiRouter.get('./ratings', function (req, res) {
+  res.json({
+    errno: 0,
+    data: ratings
+  });
+});
+
+app.use(express.static('./dist'));
+
+module.export = app.listen(port,  function (err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('Listening at http://localhost:' + port + '\n');
+});
